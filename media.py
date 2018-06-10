63b589e85ec05b65a727f40f86d8b9b82b020d0e
MOHAMED M ALTHUKAIR <40003953+thekair@users.noreply.github.com>
2018-06-10 03:30:49 +0300

    Add files via upload

--- media.py
+++ media.py
@@ -1,5 +1,5 @@
 class Movie():
-    """This class is a blueprint intended to create movie objects with similar attributes
+    """This class is intended to create movie objects with similar attributes
 
 
      Attributes:
@@ -8,16 +8,14 @@ class Movie():
         poster_image_url: A string indicating movie poster image url.
         trailer_youtube_url: A string indicating movie trailer youtube url.
     """
-    
+
     def __init__(self,
                  movie_title,
                  movie_storyline,
                  poster_image,
                  trailer_youtube):
-        """ A constructor for the movie class so that we can create instances of movie """
+        """ A constructor for the movie class to create instances of movie """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-
